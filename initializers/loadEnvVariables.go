b537de566a9bce943be00b7fdecbb5e4ac5f72fe
George <gnjuki19@gmail.com>
2024-06-13 11:46:45 +0300

    Tring to fix issues with .env file

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,12 +2,20 @@ package initializers
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
+
+	pwd, err1 := os.Getwd()
+	if err1 != nil {
+		panic(err1)
+	}
+
+	err := godotenv.Load(filepath.Join(pwd, ".env"))
 
 	if err != nil {
 		log.Fatal("FAiled to load .env file...")
